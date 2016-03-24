9909caf9208a92653e3f63c87e0b6928d56e72fb
gabrielcabral <cabraldealmeida@gmail.com>
2016-03-24 07:01:42 -0300

    fase 3

--- app/Http/Controllers/Api/Deliveryman/DeliverymanCheckoutController.php
+++ app/Http/Controllers/Api/Deliveryman/DeliverymanCheckoutController.php
@@ -2,56 +2,47 @@
 
 namespace CodeDelivery\Http\Controllers\Api\Deliveryman;
 
-use CodeDelivery\Repositories\OrderRepository;
-use CodeDelivery\Repositories\UserRepository;
-use CodeDelivery\Repositories\ProductRepository;
-use CodeDelivery\Services\OrderService;
-use Illuminate\Http\Request;
-
-use CodeDelivery\Http\Requests;
-use CodeDelivery\Http\Requests\AdminCategoryRequest;
 use CodeDelivery\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use LucaDegasperi\OAuth2Server\Facades\Authorizer;
+use CodeDelivery\Http\Requests;
 
 class DeliverymanCheckoutController extends Controller
 {
 
-    private $repository, $userRepository, $productRepository;
+    /* private $repository, $userRepository, $productRepository;
 
-    public function __construct(
-        OrderRepository $repository,
-        UserRepository $userRepository,
-        ProductRepository $productRepository,
-        OrderService $orderService
-    ){
-        $this->repository = $repository;
-        $this->userRepository = $userRepository;
-        $this->productRepository = $productRepository;
-        $this->orderService = $orderService;
-    }
+     public function __construct(
+         OrderRepository $repository,
+         UserRepository $userRepository,
+         ProductRepository $productRepository,
+         OrderService $orderService
+     ){
+         $this->repository = $repository;
+         $this->userRepository = $userRepository;
+         $this->productRepository = $productRepository;
+         $this->orderService = $orderService;
+     }
 
-    public  function index(){
-        $id = Authorizer::getResourceOwnerID();
-        $orders = $this->repository->with(['items'])->scopeQuery(function($query) use($id){
-            return $query->where('user_deliveryman_id', '=', $id);
-        })->paginate();
-        return $orders;
-    }
+     public  function index(){
+         $id = Authorizer::getResourceOwnerID();
+         $orders = $this->repository->with(['items'])->scopeQuery(function($query) use($id){
+             return $query->where('user_deliveryman_id', '=', $id);
+         })->paginate();
+         return $orders;
+     }
 
-    public function show($id){
-        $idDeliveryman = Authorizer::getResourceOwnerID();
-        return $this->repository->getByIdAndDeliveryman($id, $idDeliveryman);
-    }
+     public function show($id){
+         $idDeliveryman = Authorizer::getResourceOwnerID();
+         return $this->repository->getByIdAndDeliveryman($id, $idDeliveryman);
+     }
 
-    public  function updateStatus(Request $request, $id){
-        $idDeliveryman = Authorizer::getResourceOwnerID();
-        $order = $this->orderService->updateStatus($id, $idDeliveryman, $request->get('status'));
-        if($order != false){
-            return $order;
-        } else {
-            abort(400, 'Order não encontrado');
-        }
+     public  function updateStatus(Request $request, $id){
+         $idDeliveryman = Authorizer::getResourceOwnerID();
+         $order = $this->orderService->updateStatus($id, $idDeliveryman, $request->get('status'));
+         if($order != false){
+             return $order;
+         } else {
+             abort(400, 'Order não encontrado');
+         }
 
-    }
+     }*/
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -91,19 +91,19 @@ Route::post('oauth/access_token', function() {
 Route::group(['prefix'=>'api', 'middleware' => 'oauth', 'as' => 'api.'], function(){
 
     Route::group(['prefix' => 'client', 'middleware' => 'oauth.checkrole:client', 'as' => 'client.'], function () {
-        /*Route::resource('order', 'Api\Client\ClientCheckoutController', [
+        Route::resource('order', 'Api\Client\ClientCheckoutController', [
             'except' => ['create', 'edit', 'destroy']
-        ]);*/
+        ]);
     });
 
     Route::group(['prefix' => 'deliveryman', 'middleware' => 'oauth.checkrole:deliveryman', 'as' => 'deliveryman.'], function () {
-        /* Route::resource('order', 'Api\Deliveryman\DeliverymanCheckoutController', [
+        Route::resource('order', 'Api\Deliveryman\DeliverymanCheckoutController', [
              'except' => ['create', 'edit', 'destroy', 'store']
          ]);
 
          Route::patch('order/{id}/update-status',[
              'uses' => 'Api\Deliveryman\DeliverymanCheckoutController@updateStatus',
              'as' => 'orders.update_status'
-         ]);*/
+         ]);
     });
 });
